6465b07611b71c62f292cfac1c76031b3affa759
GabrielKishida <gabriel.kishida@usp.br>
2024-12-02 18:11:01 -0300

    :sparkles: Add IntervalState

--- fsm_config.py
+++ fsm_config.py
@@ -10,6 +10,12 @@ leds = LedsController()
 buttons = ButtonsController()
 distance = DistanceController()
 
+intervals = ["4hours", "8hours", "12hours"]
+selected_interval_index = 1
+
+sizes = ["small", "medium", "big"]
+selected_size_index = 1
+
 class MenuState(State):
     
     menu_transitions = {
@@ -29,36 +35,62 @@ class MenuState(State):
     def __init__(self):
         super().__init__("Menu State")
         self.menu_index = 0
+        texts = self.menu_texts[self.menu_index]
+        lcd.write(texts[0], texts[1])
 
     def do(self):
         if self.menu_index > 3: self.menu_index = 0
         elif self.menu_index < 0: self.menu_index = 3
-        
-        texts = self.menu_texts[self.menu_index]
-        lcd.write(texts[0], texts[1])
 
         if buttons.is_minus_pressed():
             self.menu_index -= 1
+            texts = self.menu_texts[self.menu_index]
+            lcd.write(texts[0], texts[1])
         
         if buttons.is_plus_pressed():
             self.menu_index += 1
+            texts = self.menu_texts[self.menu_index]
+            lcd.write(texts[0], texts[1])
         
         if buttons.is_confirm_pressed():
             return self.menu_transitions[self.menu_index]
 
 
 class IntervalState(State):
+    interval_texts = {
+        0: ["INTERVALO", "4 HORAS"],
+        1: ["INTERVALO", "8 HORAS"],
+        2: ["INTERVALO", "12 HORAS"],
+    }
+
+    def __init__(self):
+        super().__init__("Interval State")
+        texts = self.interval_texts[selected_interval_index]
+        lcd.write(texts[0], texts[1])
+
     def do(self):
-        print("Interval State")
+        if selected_interval_index > 2: selected_interval_index = 0
+        elif selected_interval_index < 0: selected_interval_index = 2
+
+        if buttons.is_minus_pressed():
+            selected_interval_index-= 1
+            texts = self.interval_texts[selected_interval_index]
+            lcd.write(texts[0], texts[1])
+        
+        if buttons.is_plus_pressed():
+            self.menu_index += 1
+            texts = self.interval_texts[selected_interval_index]
+            lcd.write(texts[0], texts[1])
+        
+        if buttons.is_confirm_pressed():
+            return "interval_to_menu"
 
 class SizeState(State):
     def do(self):
         print("Size State")
-
 class AutoState(State):
     def do(self):
         print("Auto State")
-
 class ManualState(State):
     def do(self):
         print("Manual State")
@@ -73,6 +105,8 @@ menu_state.add_transition("menu_to_interval", interval_state)
 menu_state.add_transition("menu_to_size", size_state)
 menu_state.add_transition("menu_to_auto", auto_state)
 menu_state.add_transition("menu_to_manual", manual_state)
+
+interval_state.add_transition("interval_to_menu", menu_state)
 
 fsm_config = FSM(menu_state)
 
